7823faece0c21f11dd2e98cde32d4029ebc34e1e
Victor <victor.baldin.2005@gmail.com>
2024-01-10 21:50:37 +0300

    started simulation

---
+++ 2023/Lagrange_simulation/src/simulator.h
@@ -0,0 +1,23 @@
+/**
+ * @file 
+ * @brief Main Simulator header.
+ * 
+ * @copyright Copyright (C) Victor Baldin, 2024.
+ */
+#ifndef SIMULATOR_H_
+#define SIMULATOR_H_
+
+#include <assert.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <SFML/Graphics.hpp>
+
+/**
+ * @brief
+ */
+
+
+#endif // SIMULATOR_H_
