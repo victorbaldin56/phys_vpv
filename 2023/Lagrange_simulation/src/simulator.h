2135bf649e6004a27207afdf4fe422f07e1deb62
Victor <victor.baldin.2005@gmail.com>
2024-01-10 21:53:05 +0300

    add simulation test

--- 2023/Lagrange_simulation/src/simulator.h
+++ 2023/Lagrange_simulation/src/simulator.h
@@ -7,14 +7,6 @@
 #ifndef SIMULATOR_H_
 #define SIMULATOR_H_
 
-#include <assert.h>
-#include <getopt.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-#include <SFML/Graphics.hpp>
-
 /**
  * @brief
  */
