00f2b6f2c72e08b4481ed6ed497f6462eb086ed4
Victor <victor.baldin.2005@gmail.com>
2024-01-13 16:58:34 +0300

    finally correct Lagrange points

--- 2023/lagrange_solver/lagrange_solver.py
+++ 2023/lagrange_solver/lagrange_solver.py
@@ -2,34 +2,60 @@
 #
 # Copyright (C) Victor Baldin, 2024.
 
+import math
 import numpy as np
-import matplotlib as plt
+import matplotlib
+from matplotlib import pyplot as plt
+
+matplotlib.use("pgf")
+matplotlib.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+})
 
 # Выбирает из массива корней действительный корень, лежащий в [min, max].
-def find_real_root(roots, min, max):
+def find_real_root(roots, rmin, rmax):
     for i in range(0, roots.__len__()):
-        if roots[i].imag == 0 and min <= roots[i].real and roots[i] <= max:
+        if math.isclose(roots[i].imag, 0) and rmin <= roots[i].real and roots[i].real <= rmax:
             return roots[i].real
+    return np.nan
 
 # Нахоит корни уравнения для данного значения alpha
 def lagrange_solve(alpha):
     roots = [0.0] * 3
     coeff = [
-        [1, 3 - alpha, 3 - 2 * alpha,    -alpha, 0,  alpha],
-        [1, 3 - alpha, 3 - 2 * alpha,     alpha, 0, -alpha],
-        [1, 3 - alpha, 3 - 2 * alpha, 2 - alpha, 0,  alpha],
+        [1, 3 - alpha, 3 - 2 * alpha,     alpha,  2 * alpha,  alpha],
+        [1, 3 - alpha, 3 - 2 * alpha,    -alpha, -2 * alpha, -alpha],
+        [1, 3 - alpha, 3 - 2 * alpha, 2 - alpha,  2 * alpha,  alpha],
     ]
-    rmin = [-1.0, 0.0   , -np.inf]
-    rmax = [ 0.0, np.inf,    -1.0]
+    rmin = [-1,      0, -np.inf]
+    rmax = [ 0, np.inf,      -1]
     for i in range(0, coeff.__len__()):
-        roots[i] = find_real_root(np.roots(coeff[i]), rmin[i], rmax[i])
+        roots[i] = find_real_root(np.roots(coeff[i]), rmin[i], rmax[i]) + 1.0 - alpha
     return roots
 
 # Вычисляем положения точек Лагранжа с шагом 0.001, вывод перенаправляем в 
 # файл. 
-print('alpha,u1,u2,u3')
+print('alpha,L1,L2,L3')
+
 alpha = 0.001
+i = 0
+roots = []
 while (alpha < 1.0):
-    roots = lagrange_solve(alpha)
-    print('{0:.3f},{1},{2},{3}'.format(alpha, roots[0], roots[1], roots[2]))
+    roots.append(lagrange_solve(alpha))
+    print('{0:.3f},{1},{2},{3}'.format(alpha, roots[i][0], roots[i][1], 
+                                       roots[i][2]))
     alpha += 0.001
+    i += 1
+
+# Графики
+alpha = [(1 + i) / 1000 for i in range(roots.__len__())]
+
+plt.figure()
+for j in range(3):
+    plt.scatter(alpha, [roots[i][j] for i in range(roots.__len__())]) 
+
+plt.grid() 
+plt.savefig('plots/all.pgf')
