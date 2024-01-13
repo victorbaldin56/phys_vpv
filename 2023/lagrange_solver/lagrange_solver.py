a2f9fd9eec6ff67c3f6728178f501908a329a947
Victor <victor.baldin.2005@gmail.com>
2024-01-13 23:29:42 +0300

    add explanations about approximation errors

--- 2023/lagrange_solver/lagrange_solver.py
+++ 2023/lagrange_solver/lagrange_solver.py
@@ -40,8 +40,7 @@ def lagrange_solve(alpha):
 # файл. 
 print('alpha,L1,L2,L3')
 
-low = [(i + 1) / 10000 for i in range(100)]
-alpha = low + [i / 1000 for i in range(10, 1000)]
+alpha = [(i + 1) / 10000 for i in range(100)] + [i / 1000 for i in range(10, 1000)]
 
 roots = []
 for i in range(alpha.__len__()):
@@ -87,3 +86,38 @@ plt.tight_layout()
 plt.xlabel('$\\alpha$')
 plt.ylabel('$x/R')
 plt.savefig('plots/subplots.pgf')
+
+fig, axs = plt.subplots(3)
+
+# Ошибки аппроксимации
+NUM_LOW_VALUES = 300
+NUM_VERY_LOW_VALUES = 200
+
+errors = [[]] * 3
+errors[0] = [abs((roots[i][0] - 1 + (alpha[i] / 3)**(1 / 3)) / roots[i][0]) 
+                   for i in range(NUM_LOW_VALUES)]
+errors[1] = [abs((roots[i][1] - 1 - (alpha[i] / 3)**(1 / 3)) / roots[i][1])
+                       for i in range(NUM_LOW_VALUES)]
+errors[2] = [abs((roots[i][2] + 1 + 5 / 12 * alpha[i]) / roots[i][2])
+                       for i in range(NUM_LOW_VALUES)]
+
+for j in range(3):
+    axs[j].scatter(alpha[:NUM_VERY_LOW_VALUES], errors[j][:NUM_VERY_LOW_VALUES], s=1.0)
+    axs[j].grid()
+    axs[j].set_title('$L_{0}'.format(j + 1))
+
+plt.tight_layout()
+
+plt.xlabel('$\\alpha$')
+plt.ylabel('$\\varepsilon$')
+plt.savefig('plots/errors.pgf')
+
+plt.figure()
+for j in range(3):
+    plt.scatter(alpha[:NUM_LOW_VALUES], errors[j], s=0.5, label='$L_{0}'.format(j + 1))
+
+plt.grid()
+plt.xlabel('$\\alpha$')
+plt.ylabel('$\\varepsilon$')
+plt.legend(markerscale=2)
+plt.savefig('plots/errors_all.pgf')
